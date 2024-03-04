e58aff8bb7eee6b5caca2409c8b4fa5a75c5267f
Josep Tormo <121088986+jtormog@users.noreply.github.com>
2024-03-04 17:23:02 +0100

    Update Barco.java

--- vehiculos2/Barco.java
+++ vehiculos2/Barco.java
@@ -1,8 +1,8 @@
-package Vehiculos_Maritimos;
+package vehiculos2;
 
 public class Barco extends Acuatico{
     static boolean motor;
-    public void setMotor{
+    public void setMotor(){
         if(motor){
             System.out.println("Tiene motor");
         }
@@ -11,7 +11,12 @@ public class Barco extends Acuatico{
         }
     }
     public Barco(String matricula, String modelo, int slora, boolean motor){
-        super(matricula,modelo,slora,motor);
+        super(matricula,modelo,slora);
         this.motor = motor;
+    }
+
+    @Override
+    public void imprimir() {
+
     }
 }
